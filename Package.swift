d749d160f1506d59287b0ccd0064f9758510ba61
Abenx <5195693+rushairer@users.noreply.github.com>
2021-10-15 00:06:55 +0800

    [Changed]Rename.

--- Package.swift
+++ Package.swift
@@ -4,17 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "Sliders",
+    name: "Sliders-SwiftUI",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Sliders",
-            targets: ["Sliders"]),
+            name: "Sliders-SwiftUI",
+            targets: ["Sliders-SwiftUI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.1"),
-        .package(url: "https://github.com/kieranb662/Shapes.git", from: "1.0.2"),
+        .package(url: "https://github.com/kieranb662/SwiftUI-Shapes.git", from: "1.0.10"),
         .package(url: "https://github.com/kieranb662/bez.git", from: "1.0.0")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Sliders",
-            dependencies: ["CGExtender", "Shapes", "bez"]),
+            name: "Sliders-SwiftUI",
+            dependencies: ["CGExtender", .product(name: "Shapes", package: "SwiftUI-Shapes"), "bez"]),
         .testTarget(
             name: "SlidersTests",
-            dependencies: ["Sliders"]),
+            dependencies: ["Sliders-SwiftUI"]),
     ]
 )
